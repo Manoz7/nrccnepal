dccc1071c9171e5d64661f268a8ed45342d0b2b2
Manoz7 <manojkhanal936@gmail.com>
2023-03-09 22:25:40 +0545

    हर हर महादेव:

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def blog(request):
+	return render(request, "blog/blog-page.html")
+
+
+def single_blog(request):
+	return render(request, "blog/single-blog.html")
